9fa849f29d26235f08156f2efb19eda971dcaafa
benNjogu <kamandemachafi@gmail.com>
2023-01-04 11:26:40 +0300

    CTA

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import "./cta.css";
 
+//Click To Action
 const CTA = () => {
-  return <div>CTA</div>;
+  return (
+    <div className="gpt3__cta">
+      <div className="gpt3__cta-content">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register Today & start exploring the endless possibilities.</h3>
+      </div>
+      <div className="gpt3__cta-btn">
+        <button>Get Started</button>
+      </div>
+    </div>
+  );
 };
 
 export default CTA;
